0e936a21f884f14ce59aba83835fbd2964215c35
Shiran Cohen <shiran28030@gmail.com>
2018-07-28 18:35:31 +0300

    languageOptions
    add error if the language is not english

--- src/app/components/container/welcome-screen/welcome-screen.component.ts
+++ src/app/components/container/welcome-screen/welcome-screen.component.ts
@@ -8,11 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeScreenComponent implements OnInit {
 
-
+  languageOptions : Array<string> = ["Engilsh","French","German","Italian"];
+  isNotEnglish: boolean;
+  
   constructor() { }
 
   ngOnInit() {
-
+    this.isNotEnglish = false;
   }
+
+  changeOption(option){
+    if(option !== "English"){
+      this.isNotEnglish = true;
+    }
+    else{
+      this.isNotEnglish = false;
+    }
+  }
+
+
 
 }
